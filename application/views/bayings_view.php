e47c13aba739311af4132fef714249e37168c107
Bogomaz <bogomassm@gmail.com>
2016-04-29 14:35:16 +0300

    2016-04-29 Pilot
    Здесь:
    - реализована mvc модель в самом базовом её варианте
    - свёрстана одна страничка, в которой ещё нет настоящего контента (только рыба lorem ipsum)

---
+++ application/views/bayings_view.php
@@ -0,0 +1,32 @@
+<link rel = "stylesheet" href = "css/main_style.css" >
+<table>
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Товар</td>
+    </tr>
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Хлеб</td>
+    </tr>
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Молоко</td>
+    </tr> 
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Яблоки</td>
+    </tr>   
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Груши</td>
+    </tr>   
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Морковь</td>
+    </tr>  
+    <tr>
+        <td type = "checkbox"></td>
+        <td>Лук</td>
+    </tr>      
+</table>
+
